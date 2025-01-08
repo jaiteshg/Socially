9025893508d1b4e170694199c4edd37641b6b6be
jaiteshg <gjaitesh@gmail.com>
2025-01-08 19:45:36 +0530

    Project completed

--- src/components/DesktopNavbar.tsx
+++ src/components/DesktopNavbar.tsx
@@ -2,9 +2,8 @@ import { BellIcon, HomeIcon, UserIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { SignInButton, UserButton } from "@clerk/nextjs";
-import ModeToggle from "./ModelToggel";
+import ModeToggle from "./ModeToggle";
 import { currentUser } from "@clerk/nextjs/server";
-
 
 async function DesktopNavbar() {
   const user = await currentUser();
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ import { syncUser } from "@/actions/user.action";
 
 async function Navbar() {
   const user = await currentUser();
-  if (user) await syncUser(); // POST
+  if (user) await syncUser(); 
 
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
